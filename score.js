0633b64ed9ae1fdc097115fcedf1b4e0a502e066
Fahad Sadah <fahadsadah@gmail.com>
2010-08-27 19:53:01 +0100

    Fixed URL

--- score.js
+++ score.js
@@ -4,5 +4,5 @@ function score_add(value) {
 	var scorehole = document.getElementById('score');
 	scorehole.innerHTML = score;
 	var submit = document.getElementById('submit');
-	submit.href = 'http://twitter.com/?status=' + escape('I just scored ' + score + ' at http://2tu.us/2m07');
+	submit.href = 'http://twitter.com/?status=' + escape('I just scored ' + score + ' at http://2tu.us/2m0d');
 }

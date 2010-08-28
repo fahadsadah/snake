94d7805454e23674ab127b2dc7f0a524b1ec9711
Fahad Sadah <fahadsadah@gmail.com>
2010-08-28 10:57:44 +0100

    Win

--- score.js
+++ score.js
@@ -5,4 +5,7 @@ function score_add(value) {
 	scorehole.innerHTML = score;
 	var submit = document.getElementById('submit');
 	submit.href = 'http://twitter.com/?status=' + escape('I just scored ' + score + ' at http://2tu.us/2m0d');
+	if (score > 9000) {
+		submit.href = 'http://twitter.com/?status=' + escape('I just scored OVAR 9000 at http://2tu.us/2m0d!');
+	}
 }

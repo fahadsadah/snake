42c2a295c40311f84712e51ca2b59fcd02031745
Fahad Sadah <fahadsadah@gmail.com>
2011-12-28 12:59:06 +0000

    fixed setTimeout syntax

--- main.js
+++ main.js
@@ -41,11 +41,11 @@ function loop() {
 			if (wait < 10) {
 				wait = 10;
 			}
-			setTimeout('loop()', wait);
+			setTimeout(loop, wait);
 		}
 	}
 	else {
-		setTimeout('loop()', 50);
+		setTimeout(loop, 50);
 	}
 }
 loop();

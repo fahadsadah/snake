9c3723caef3b91a8ca5b15f96b7435f4a8b77de2
Fahad Sadah <fahadsadah@gmail.com>
2012-12-14 00:30:50 +0000

    DOH! Fix dumb reference error

--- score.js
+++ score.js
@@ -4,9 +4,9 @@ function score_add(value) {
 	var scorehole = document.getElementById('score');
 	scorehole.innerHTML = score;
 	var submit = document.getElementById('submit');
-	submit.href = 'http://twitter.com/?status=' + escape('I just scored ' + score + ' at http://2tu.us/2m0d');
+//	submit.href = 'http://twitter.com/?status=' + escape('I just scored ' + score + ' at http://2tu.us/2m0d');
 	if (score > 9000) {
-		submit.href = 'http://twitter.com/?status=' + escape('I just scored OVAR 9000 at http://2tu.us/2m0d!');
+//		submit.href = 'http://twitter.com/?status=' + escape('I just scored OVAR 9000 at http://2tu.us/2m0d!');
 	}
 }
 function score_get() {

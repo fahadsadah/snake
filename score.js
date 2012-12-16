4114ba65362237e7c783ea9ddd115e11a824f541
Fahad Sadah <fahadsadah@gmail.com>
2012-12-16 01:20:39 +0000

    Redesign!
    Added legend, moved score back. Not too great a redesign

--- score.js
+++ score.js
@@ -3,11 +3,6 @@ function score_add(value) {
 	score = score + value;
 	var scorehole = document.getElementById('score');
 	scorehole.innerHTML = score;
-	var submit = document.getElementById('submit');
-//	submit.href = 'http://twitter.com/?status=' + escape('I just scored ' + score + ' at http://2tu.us/2m0d');
-	if (score > 9000) {
-//		submit.href = 'http://twitter.com/?status=' + escape('I just scored OVAR 9000 at http://2tu.us/2m0d!');
-	}
 }
 function score_get() {
 	return score;

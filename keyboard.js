4bfadd6daaa44bd7b870c20c948fa0571ad7feb8
Fahad Sadah <fahadsadah@gmail.com>
2012-12-21 23:34:15 +0000

    Added WASD support

--- keyboard.js
+++ keyboard.js
@@ -1,8 +1,15 @@
 var directionqueue = [];
+var alt = []; alt[87] = 38; alt[65] = 37; alt[83] = 40; alt[68] = 39;
 document.onkeydown = function(event) {
-	if (event.keyCode >= 37 && event.keyCode <= 40) {
-		if (directionqueue[directionqueue.length -1] != event.keyCode) {
-			directionqueue.push(event.keyCode);
+	if (!(event.keyCode >= 37 && event.keyCode <= 40) && alt[event.keyCode] >= 37 && alt[event.keyCode] <= 40) {
+		e = alt[event.keyCode];
+	}
+	else {
+		e = event.keyCode;
+	}
+	if (e >= 37 && e <= 40) {
+		if (directionqueue[directionqueue.length -1] != e) {
+			directionqueue.push(e);
 		}
 	}
 }

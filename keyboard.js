bd77e06bdb6598f2949c85eb2c29529feb4554e0
Fahad Sadah <fahadsadah@gmail.com>
2012-12-23 15:51:59 +0000

    Rewrote input code

--- keyboard.js
+++ keyboard.js
@@ -1,16 +1,12 @@
 var directionqueue = [];
-var alt = []; alt[87] = 38; alt[65] = 37; alt[83] = 40; alt[68] = 39;
 document.onkeydown = function(event) {
-	if (!(event.keyCode >= 37 && event.keyCode <= 40) && alt[event.keyCode] >= 37 && alt[event.keyCode] <= 40) {
-		e = alt[event.keyCode];
-	}
-	else {
-		e = event.keyCode;
-	}
-	if (e >= 37 && e <= 40) {
-		if (directionqueue[directionqueue.length -1] != e) {
-			directionqueue.push(e);
-		}
+	e = event.keyCode;
+	if (e == 65) { e = 37; }
+	if (e == 87) { e = 38; }
+	if (e == 68) { e = 39; }
+	if (e == 83) { e = 40; }
+	if (e >= 37 && e <= 40 && directionqueue[directionqueue.length -1] != e) {
+		directionqueue.push(e);
 	}
 }
 
